f352d83ba9edf74e1eb30c74dfa82944781ad397
Douglas dos Santos <douglas.santos@softplan.com.br>
2019-05-15 16:53:01 -0300

    Inserindo o Template e criando o cadastro de Cerveja

--- brewer/src/main/java/br/com/idsolucoesdigitais/brewer/controllers/CervejasController.java
+++ brewer/src/main/java/br/com/idsolucoesdigitais/brewer/controllers/CervejasController.java
@@ -5,9 +5,8 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.idsolucoesdigitais.brewer.model.Cerveja;
@@ -15,14 +14,12 @@ import br.com.idsolucoesdigitais.brewer.model.Cerveja;
 @Controller
 public class CervejasController {
 	
-	@GetMapping
 	@RequestMapping("/cervejas/novo")
 	public String novo(Cerveja cerveja) {
 		return "cerveja/CadastroCerveja";
 	}
 	
-	@PostMapping
-	@RequestMapping(value = "/cervejas/novo" )
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
 	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			return novo(cerveja);
@@ -32,6 +29,11 @@ public class CervejasController {
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		System.out.println(">>> sku: " + cerveja.getSku());
 		return "redirect:/cervejas/novo";
+	}
+	
+	@RequestMapping("/cervejas/cadastro")
+	public String cadastro() {
+		return "cerveja/cadastro-produto";
 	}
 	
 }
